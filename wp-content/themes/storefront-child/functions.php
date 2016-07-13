e53dedc1edd1836cd522006e2da0369a5f06021f
Matt Miller <matt@miller-media.com>
2016-07-12 20:52:23 -0700

    Removed tabs on WooCommerce product page

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -55,3 +55,15 @@ function custom_storefront_credit() {
     </div>
     <?php
 }
+
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+
+function woo_remove_product_tabs( $tabs ) {
+
+    unset( $tabs['description'] );      	// Remove the description tab
+    unset( $tabs['reviews'] ); 			// Remove the reviews tab
+    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+
+    return $tabs;
+
+}
